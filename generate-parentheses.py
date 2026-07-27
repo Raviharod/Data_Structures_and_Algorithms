10447fa350161437900dd31d5bed22ffb1a1c71a
Ravi Harod <raviharod7828@gmail.com>
2026-07-27 11:04:35 +0530

    generate all parantheses

--- generate-parentheses.py
+++ generate-parentheses.py
@@ -1,3 +1,30 @@
 #Advance Recursion
 #Generate Parenthesis
 
+def generate_parentheses(n):
+  ind = 0
+  total = 0
+  brackets = [""]*n*2
+  result = []
+  def solve(ind, total):
+    if ind >= len(brackets):
+      if total == 0:
+        result.append("".join(brackets))
+      return
+    if total>len(brackets)//2:
+      return
+    elif total < 0:
+      return
+    brackets[ind] = "("
+    sum = total+1
+    solve(ind+1, sum)
+    brackets[ind] = ")"
+    sum = total -1
+    solve(ind+1, sum)
+
+  solve(ind, total)
+  return result
+
+print(generate_parentheses(3))
+
+
